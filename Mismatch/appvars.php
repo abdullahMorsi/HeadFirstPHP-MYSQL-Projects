4be2c4fa9aeca7b0e50b44a2968e776c7b74af8e
abdullahMorsi <abdoalnegm69@gmail.com>
2022-06-02 19:30:51 +0200

    First commit

---
+++ Mismatch/appvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define application constants
+  define('MM_UPLOADPATH', 'images/');
+  define('MM_MAXFILESIZE', 327680000);      // 32 KB
+  define('MM_MAXIMGWIDTH', 4000000);        // 120 pixels
+  define('MM_MAXIMGHEIGHT', 4000000);       // 120 pixels
+?>
